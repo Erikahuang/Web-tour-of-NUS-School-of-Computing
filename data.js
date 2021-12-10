bb3af13a7d3bc03d618b75d4d7b275141ad11881
Erikahuang <78543608+Erikahuang@users.noreply.github.com>
2021-12-10 15:43:12 +0800

    First commit

---
+++ data.js
@@ -0,0 +1,176 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-com1-building_edited",
+      "name": "COM1 Building_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.0867671351481345,
+        "pitch": -0.21777147228887905,
+        "fov": 1.513843149153066
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.391803649664233,
+          "pitch": 0.025997693870694505,
+          "rotation": 0,
+          "target": "2-lobby_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2440041315823951,
+          "pitch": -0.06531260905085468,
+          "title": "NUS School of computing",
+          "text": "Hello! Nice to meet you at COM1 building of School of Computing🥰<div>Let's explore the world of computing!</div>"
+        },
+        {
+          "yaw": -1.645113888789627,
+          "pitch": -0.04763889330465432,
+          "title": "The Deck's",
+          "text": "Go straight, eat at the Deck's, a canteen that is nearest to COM1!"
+        },
+        {
+          "yaw": 1.3967767284041877,
+          "pitch": 0.009314458335333953,
+          "title": "COM2 Building",
+          "text": "Go this way, if you want to go to COM2."
+        },
+        {
+          "yaw": -2.8317768017869493,
+          "pitch": -0.08969684326689809,
+          "title": "Carpark",
+          "text": "Free parking area."
+        }
+      ]
+    },
+    {
+      "id": "1-foyer-at-level-2_edited",
+      "name": "Foyer at Level 2_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.6121729030554839,
+        "pitch": 0.0024407122997800457,
+        "fov": 1.421937074762129
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7004503120223724,
+          "pitch": 0.23356779517433068,
+          "rotation": 0,
+          "target": "2-lobby_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.083223417841353,
+          "pitch": 0.3109007461449167,
+          "title": "Foyer at level 2",
+          "text": "Take a seat and start to study!<div>A water cooler is at the end of this corridor.<br><div><br></div></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-lobby_edited",
+      "name": "Lobby_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.6553129018635886,
+        "pitch": 0.04453818364164164,
+        "fov": 1.4122244971703988
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4236099287992072,
+          "pitch": 0.0716597955110938,
+          "rotation": 0,
+          "target": "1-foyer-at-level-2_edited"
+        },
+        {
+          "yaw": 1.32452468877994,
+          "pitch": 0.18640957839196304,
+          "rotation": 0,
+          "target": "0-com1-building_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0410936802112705,
+          "pitch": 0.2979680470030921,
+          "title": "&nbsp;Lobby of COM1",
+          "text": "Welcome in!<div>If you want to study, please go for level 2 following the direction instruction. There is a quiet reading area for you!</div><div>Having any question? Feel free to ask our staff at front desk!😀</div>"
+        },
+        {
+          "yaw": -2.1231644507238947,
+          "pitch": 0.12875395621501973,
+          "title": "Office of Computing",
+          "text": "Make sure that you are bring the student card or staff card, or you can not make a entry."
+        }
+      ]
+    }
+  ],
+  "name": "NUS School of Computing",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
